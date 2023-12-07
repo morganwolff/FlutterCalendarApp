8dbc914e9bfaaef199517c94d937c7b77804e5d9
Morgan Wolff <morgan.wolff@instant-system.com>
2023-12-07 17:34:24 +0900

    Debug +1

--- lib/Pages/calendar_page/views/CalendarEventDetailsPage.dart
+++ lib/Pages/calendar_page/views/CalendarEventDetailsPage.dart
@@ -20,114 +20,142 @@ class EventDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     var provider = Provider.of<CalendarEventProvider>(context);
     Map<String, int> indexes = provider.findAllMeetingOccurrences(uuid);
-    return Scaffold(
-      appBar: AppBar(
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.edit_outlined),
-            onPressed: () {
-              provider.loadMeetingData(uuid);
+    if (indexes.isNotEmpty) {
+      return Scaffold(
+        appBar: AppBar(
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.edit_outlined),
+              onPressed: () {
+                provider.loadMeetingData(uuid);
 
-              showModalBottomSheet(
-                context: context,
-                builder: (context) => MyCustomBottomSheet(meetingUuid: uuid),
-              );
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.delete_outline),
-            onPressed: () {
-              provider.deleteMeeting(uuid);
-              Navigator.pop(context);
-            },
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 5.0),
-                  child: ColoredBox(
-                    color: (provider.meetingsMap[provider.selectedCalendar]![indexes[provider.selectedCalendar]!].background),
-                    child: const SizedBox(height: 30, width: 30),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10.0, top: 5.0),
-                  child: Column(
-                    children: [
-                      Text(
-                          (provider.meetingsMap[provider.selectedCalendar]![indexes[provider.selectedCalendar]!].title),
-                          style: const TextStyle(fontSize: 26, fontWeight: FontWeight.bold)
-                      ),
-                    ],
-                  ),
-                )
-              ],
+                showModalBottomSheet(
+                  context: context,
+                  builder: (context) => MyCustomBottomSheet(meetingUuid: uuid),
+                );
+              },
             ),
-            SizedBox(height: 20),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(top: 5.0),
-                  child: Align(
-                    alignment: Alignment.topCenter,
-                    child: Icon(Icons.access_time_outlined),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10.0, top: 5.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        (provider.meetingsMap[provider.selectedCalendar]![indexes[provider.selectedCalendar]!].isAllDay)
-                            ? DateFormat('EEE, MM/dd/y').format((provider.meetingsMap[provider.selectedCalendar]![indexes[provider.selectedCalendar]!].from))
-                            : DateFormat('EEE, MM/dd/y').add_jm().format((provider.meetingsMap[provider.selectedCalendar]![indexes[provider.selectedCalendar]!].from)),
-                        style: const TextStyle(fontSize: 16,  fontWeight: FontWeight.bold),
-                      ),
-
-                      Text(
-                        (provider.meetingsMap[provider.selectedCalendar]![indexes[provider.selectedCalendar]!].isAllDay)
-                            ? ''
-                            : DateFormat('EEE, MM/dd/y').add_jm().format((provider.meetingsMap[provider.selectedCalendar]![indexes[provider.selectedCalendar]!].to)),
-                        style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-            SizedBox(height: 20),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(top: 5.0),
-                  child: Align(
-                    alignment: Alignment.topCenter,
-                    child: Icon(Icons.menu),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 10.0, top: 5.0),
-                    child: Text(
-                        (provider.meetingsMap[provider.selectedCalendar]![indexes[provider.selectedCalendar]!].description),
-                        softWrap: true, style: TextStyle(fontSize: 16)),
-                  ),
-                ),
-              ],
+            IconButton(
+              icon: const Icon(Icons.delete_outline),
+              onPressed: () {
+                Navigator.pop(context);
+                provider.deleteMeeting(uuid);
+              },
             ),
           ],
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5.0),
+                    child: ColoredBox(
+                      color: (provider.meetingsMap[provider
+                          .selectedCalendar]![indexes[provider
+                          .selectedCalendar]!].background),
+                      child: const SizedBox(height: 30, width: 30),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0, top: 5.0),
+                    child: Column(
+                      children: [
+                        Text(
+                            (provider.meetingsMap[provider
+                                .selectedCalendar]![indexes[provider
+                                .selectedCalendar]!].title),
+                            style: const TextStyle(
+                                fontSize: 26, fontWeight: FontWeight.bold)
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 20),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 5.0),
+                    child: Align(
+                      alignment: Alignment.topCenter,
+                      child: Icon(Icons.access_time_outlined),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0, top: 5.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          (provider.meetingsMap[provider
+                              .selectedCalendar]![indexes[provider
+                              .selectedCalendar]!].isAllDay)
+                              ? DateFormat('EEE, MM/dd/y').format((provider
+                              .meetingsMap[provider
+                              .selectedCalendar]![indexes[provider
+                              .selectedCalendar]!].from))
+                              : DateFormat('EEE, MM/dd/y').add_jm().format(
+                              (provider.meetingsMap[provider
+                                  .selectedCalendar]![indexes[provider
+                                  .selectedCalendar]!].from)),
+                          style: const TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+
+                        Text(
+                          (provider.meetingsMap[provider
+                              .selectedCalendar]![indexes[provider
+                              .selectedCalendar]!].isAllDay)
+                              ? ''
+                              : DateFormat('EEE, MM/dd/y').add_jm().format(
+                              (provider.meetingsMap[provider
+                                  .selectedCalendar]![indexes[provider
+                                  .selectedCalendar]!].to)),
+                          style: const TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 20),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 5.0),
+                    child: Align(
+                      alignment: Alignment.topCenter,
+                      child: Icon(Icons.menu),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 10.0, top: 5.0),
+                      child: Text(
+                          (provider.meetingsMap[provider
+                              .selectedCalendar]![indexes[provider
+                              .selectedCalendar]!].description),
+                          softWrap: true, style: TextStyle(fontSize: 16)),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return Scaffold(
+        body: Text("No event"),
+      );
+    }
   }
 }
