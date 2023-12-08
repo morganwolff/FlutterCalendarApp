f79b54e5f81a34ce05d56269e19e8391d065d742
Morgan Wolff <morgan.wolff@epitech.eu>
2023-12-08 16:33:07 +0900

    Refacto settings page and add translation

--- lib/pages/settings_page/views/SettingsPage.dart
+++ lib/pages/settings_page/views/SettingsPage.dart
@@ -1,45 +1,8 @@
-/*import 'package:flutter/material.dart';
-import 'package:flutter_localization/flutter_localization.dart';
-
-import '../../../locals/app_locale.dart';
-import '../../../locals/local_storage.dart';
-
-class SettingsPage extends StatelessWidget {
-  const SettingsPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            const Text("Settings Page"),
-            ElevatedButton(onPressed: () {
-              if (AppLocale.currentLanguage.getString(context) == "en") {
-                FlutterLocalization.instance.translate("fr");
-              } else {
-                FlutterLocalization.instance.translate("en");
-              }
-            }, child: const Text("change language")),
-          ],
-        ),
-      ),
-    );
-  }
-}
-*/
 import 'package:flutter/material.dart';
 import 'package:flutter_localization/flutter_localization.dart';
 import 'package:flutter_calendar_app/locals/app_locale.dart';
-import 'package:flutter_calendar_app/components/textFieldLoginSubscribe.dart';
 import 'package:provider/provider.dart';
 import '../../../components/themes.dart';
-import '../../../locals/local_storage.dart';
-import '../../../locals/app_locale.dart';
-import '../../login/views/Login.dart';
 
 class SettingsPage extends StatefulWidget {
   SettingsPage({Key? key});
@@ -57,57 +20,49 @@ class _SettingsPageState extends State<SettingsPage> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(AppLocale.currentLocale[AppLocale.settings]),
+        title: Text(AppLocale.settings.getString(context)),
         backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
-
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  AppLocale.currentLocale[AppLocale.select_lang],
-                  style: TextStyle(fontSize: 15),
-                ),
-                DropdownButton<String>(
-                  value: selectedLanguage,
-                  onChanged: (String? newValue) {
-                    if (newValue != null) {
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(AppLocale.select_lang.getString(context),),
+                  DropdownButton<String>(
+                    value: AppLocale.currentLanguage.getString(context),
+                    onChanged: (String? newValue) {
                       setState(() {
+                        FlutterLocalization.instance.translate(newValue!);
                         selectedLanguage = newValue;
-                        AppLocale.setLocale(selectedLanguage);
                       });
-                    }
-                  },
-                  items: AppLocale.supportedLanguages.map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                ),
-              ],
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
-                );
-              },
-              child: Text(AppLocale.currentLocale[AppLocale.log_in]),
-            ),
-            Switch(
-              value: themeProvider.isDarkMode,
-              onChanged: (value) {
-                themeProvider.toggleTheme();
-              },
-            ),
-            Text('Dark Mode'),
-          ],
+                    },
+                    items: AppLocale.supportedLanguages.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("${AppLocale.change_theme.getString(context)}    ${themeProvider.isDarkMode ? AppLocale.theme_dark.getString(context) : AppLocale.theme_light.getString(context)}"),
+                  Switch(
+                    value: themeProvider.isDarkMode,
+                    onChanged: (value) {
+                      themeProvider.toggleTheme();
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
